3d120328b41bb703a5447e83e63fe77b30db0a00
trulybruly <66429941+trulybruly@users.noreply.github.com>
2022-08-05 17:50:39 +0300

    fix3

--- debian/deb.sh
+++ debian/deb.sh
@@ -23,6 +23,7 @@ sudo apt install nnn
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #autosuggestions and syntax-highlighting
+cd ${HOME}
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 #.zshrc
@@ -35,4 +36,4 @@ _zshinstall=${HOME}/.oh-my-zsh
 sed -i '1s@^@export ZSH="'$_zshinstall'"\n@' ${HOME}/.zshrc
 sed -i '1s@^@#Path to your oh-my-zsh installation\n@' ${HOME}/.zshrc
 
-source ${HOME}.zshrc
+source ${HOME}/.zshrc
